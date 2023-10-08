d5dd3b3896fa40a1af4726fa7179d85678818480
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-10-08 03:46:45 +0000

    [Add]before_action（退会）

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,19 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+    # 【処理１】入力されたemailからアカウントを１件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    # アカウントを取得できなかった場合、このメソッドを修了する
+    return if !@customer
+    # 【処理２】取得したアカウントのパスワードと入力されたパスワードが一致しかつ退会済みかを判別
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+      # 【処理３】退会済みの場合、サインアップページへリダイレクトする
+      redirect_to new_customer_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
