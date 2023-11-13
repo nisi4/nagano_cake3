1fd73c71508b0cd6bf3f6b4963073cc462724316
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-11-13 13:21:37 +0000

    [Finish]admin_item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,6 +19,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_show_path(@item.id)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     get 'items/index'
     get 'items/:id/show' => "items#show",as: "item_show"
     get 'items/:id/edit' => "items#edit",as: "item_edit"
+    patch "items/:id" => "items#update",as: "item_update"
   end
   namespace :admin do
     get 'homes/top'
